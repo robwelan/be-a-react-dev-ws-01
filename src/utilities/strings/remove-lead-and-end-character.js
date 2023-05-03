0adaa83cbde4d837895752ac31b8a4d1c919cabb
Rob Welan <rmw.its@gmail.com>
2023-05-03 18:42:31 +1000

    New utility
     - utility removes a character at beginning and end of  a string
     - everything is customisable

---
+++ src/utilities/strings/remove-lead-and-end-character.js
@@ -0,0 +1,22 @@
+const getRemoveLeadAndEndCharacter = (payload = {}) => {
+  const { value, character = {} } = payload
+  const { end = "", lead = "" } = character
+
+  if (value === "" || end === "" || lead === "") {
+    return value
+  }
+
+  const valueArray = value.split("")
+
+  if (valueArray[0] === lead) {
+    valueArray.shift()
+  }
+
+  if (valueArray.at(-1) === end) {
+    valueArray.pop()
+  }
+
+  return valueArray.join("")
+}
+
+export default getRemoveLeadAndEndCharacter
