86e47440a353ca79e9b37da83f5d8eabe2b3066e
Rob Welan <rmw.its@gmail.com>
2023-05-08 08:50:39 +1000

    Update manifest in gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -152,14 +152,14 @@ module.exports = {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `Be A React Dev`,
-        short_name: `Gatsby`,
+        short_name: `beareactdev`,
         start_url: `/`,
         background_color: `#ffffff`,
         // This will impact how browsers show your PWA/website
         // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
+        // theme_color: `#ffde59`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/beareactdev_icon.svg`, // This path is relative to the root of the site.
       },
     },
   ],
