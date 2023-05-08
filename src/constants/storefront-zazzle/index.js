462725a4462c4c21c0ff39122734cb816b71683c
Rob Welan <rmw.its@gmail.com>
2023-05-08 16:21:55 +1000

    Update store front

--- src/constants/storefront-zazzle/index.js
+++ src/constants/storefront-zazzle/index.js
@@ -1,6 +1,13 @@
 import tshirtMenAutisticAsFuck from './tshirt-men-autistic-as-fuck';
 import tshirtWomenAutisticAsFuck from './tshirt-women-autistic-as-fuck';
+import tshirtMenWistful from './tshirt-men-wistful';
+import tshirtWomenWistful from './tshirt-women-wistful';
 
-const storefront = [tshirtMenAutisticAsFuck, tshirtWomenAutisticAsFuck];
+const storefront = [
+  tshirtMenAutisticAsFuck,
+  tshirtMenWistful,
+  tshirtWomenAutisticAsFuck,
+  tshirtWomenWistful,
+];
 
 export default storefront;
