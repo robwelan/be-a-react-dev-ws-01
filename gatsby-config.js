a0112baca4b6209ebb25c05ec6c7b8a214866fa8
Rob Welan <rmw.its@gmail.com>
2023-05-10 13:04:36 +1000

    Update chakra-ui packages and gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,7 +24,26 @@ module.exports = {
     },
   },
   plugins: [
-    '@chakra-ui/gatsby-plugin',
+    {
+      resolve: '@chakra-ui/gatsby-plugin',
+      options: {
+        /**
+         * @property {boolean} [resetCSS=true]
+         * if false, this plugin will not use `<CSSReset />
+         */
+        resetCSS: true,
+        /**
+         * @property {boolean} [isUsingColorMode=true]
+         * if false, this plugin will not use <ColorModeProvider />
+         */
+        isUsingColorMode: true,
+        /**
+         * @property {boolean} [isBaseProvider=false]
+         * if true, will render `<ChakraBaseProvider>`
+         */
+        isBaseProvider: false,
+      },
+    },
     {
       resolve: `gatsby-plugin-google-gtag`,
       options: {
