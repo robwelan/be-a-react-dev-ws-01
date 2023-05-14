00e8031fb980076dad17a16b01dd772750111c61
Rob Welan <rmw.its@gmail.com>
2023-05-14 19:58:53 +1000

    Update 404 page
     - make nicer

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,38 @@
-import * as React from 'react';
-import { graphql } from 'gatsby';
+import React from 'react';
+//  chakra-ui
+import { Box, Heading, Text, Button } from '@chakra-ui/react';
 //  components
 import Seo from '../components/seo';
 
 const NotFoundPage = ({ location }) => {
   return (
     <>
-      <h1>404: Not Found</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <Box textAlign="center" py={10} px={6}>
+        <Heading
+          display="inline-block"
+          as="h2"
+          size="2xl"
+          bgGradient="linear(to-r, teal.400, teal.600)"
+          backgroundClip="text"
+        >
+          404
+        </Heading>
+        <Text fontSize="18px" mt={3} mb={2}>
+          Page Not Found
+        </Text>
+        <Text color={'gray.500'} mb={6}>
+          The page you're looking for does not seem to exist
+        </Text>
+
+        <Button
+          colorScheme="teal"
+          bgGradient="linear(to-r, teal.400, teal.500, teal.600)"
+          color="white"
+          variant="solid"
+        >
+          Go to Home
+        </Button>
+      </Box>
     </>
   );
 };
@@ -15,4 +40,3 @@ const NotFoundPage = ({ location }) => {
 export const Head = () => <Seo title="404: Not Found" />;
 
 export default NotFoundPage;
-
