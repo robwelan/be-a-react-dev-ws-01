0d5f72c12750a61cc4c31995d47cd04a42730866
Rob Welan <rmw.its@gmail.com>
2023-06-28 14:38:02 +1000

    Update about
     - add my-projects

---
+++ src/content-pages/about-page/my-projects.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+//  chakra-ui
+import {
+  Heading,
+  Link,
+  List,
+  ListItem,
+  ListIcon,Text,
+  VStack,
+} from '@chakra-ui/react';
+//  chakra-ui icons
+import { ExternalLinkIcon } from '@chakra-ui/icons';
+//  react icons
+import { FaGlobe } from '@react-icons/all-files/fa/FaGlobe';
+
+const MyProjects = () => (
+  <VStack alignItems="flex-start" marginTop="4em" spacing={2} w="100%">
+    <Heading as="h2" size="sm">
+      GatsbyJS Projects
+    </Heading>
+    <Text>The following are projects I have designed, developed, host and manage on behalf of clients.</Text>
+    <List spacing={2}>
+      <ListItem>
+        <ListIcon as={FaGlobe} color="green.700" />
+        <Link href="https://almuccienterprises.com/" isExternal>
+          Al Mucci Enterprises <ExternalLinkIcon />
+        </Link>
+      </ListItem>
+
+      <ListItem>
+        <ListIcon as={FaGlobe} color="green.700" />
+        <Link href="https://ipswicho2.com.au/" isExternal>
+          Ipswich O2 <ExternalLinkIcon />
+        </Link>
+      </ListItem>
+
+      <ListItem>
+        <ListIcon as={FaGlobe} color="green.700" />
+        <Link href="https://wealthier-you.com.au/" isExternal>
+          Wealthier Y♀u <ExternalLinkIcon />
+        </Link>
+      </ListItem>
+    </List>
+  </VStack>
+);
+
+export default MyProjects;
