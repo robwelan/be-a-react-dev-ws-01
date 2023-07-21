6b9d2f44138df71fa751b2f945b550800e539f13
Rob Welan <rmw.its@gmail.com>
2023-07-21 11:03:07 +1000

    Update zazzle store

--- src/constants/storefront-zazzle/sticker-designs/index.js
+++ src/constants/storefront-zazzle/sticker-designs/index.js
@@ -1,5 +1,6 @@
 import stickerBeAReactDevSmall from './sticker-be-a-react-dev-small';
+import stickerNeurodivergent from './sticker-neurodivergent';
 
-const products = [stickerBeAReactDevSmall];
+const products = [stickerBeAReactDevSmall, stickerNeurodivergent];
 
 export default products;
---
+++ src/constants/storefront-zazzle/sticker-designs/sticker-neurodivergent.js
@@ -0,0 +1,19 @@
+/*
+  <div style="text-align:center;line-height:150%"> <a href="https://www.zazzle.com.au/neurodivergent_vinyl_stickers-256376456487046608" rel="nofollow" > <img src="https://rlv.zcache.com.au/neurodivergent_vinyl_stickers-r11b74a5948a24208af081e73ca7429fd_08m3c_325.jpg?rlvnet=1&bg=0xffffff" alt="neurodivergent Vinyl Stickers" style="border:0;" /> </a> <br /> <a href="https://www.zazzle.com.au/neurodivergent_vinyl_stickers-256376456487046608" rel="nofollow" >neurodivergent Vinyl Stickers</a> <br />by <a href="https://www.zazzle.com.au/store/beareactdev" rel="nofollow">beareactdev</a> </div>
+*/
+
+const sticker = {
+  asset: {
+    image: {
+      alt: 'neurodiverent',
+      src: 'https://rlv.zcache.com.au/neurodivergent_vinyl_stickers-r11b74a5948a24208af081e73ca7429fd_08m3c_325.jpg?rlvnet=1&bg=0xffffff',
+    },
+    name: 'Awesome neurodiverent Stickers',
+    uri: 'https://www.zazzle.com.au/neurodivergent_vinyl_stickers-256376456487046608',
+  },
+  // gender: 'male',
+  name: 'neurodiverent',
+  type: 'sticker',
+};
+
+export default sticker;
