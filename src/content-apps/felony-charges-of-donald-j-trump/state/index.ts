165774fdbb46859df936d3708d48aa4ef7af50d0
Rob Welan <rmw.its@gmail.com>
2023-09-08 08:37:52 +1000

    Update apps felony charges of donald j trump
     - move files to logical locations
     - add an atom because ContainerDimensions is loading too soon

---
+++ src/content-apps/felony-charges-of-donald-j-trump/state/index.ts
@@ -0,0 +1,3 @@
+import { felonyHeight } from './atoms';
+//
+export { felonyHeight };
---
+++ src/content-apps/felony-charges-of-donald-j-trump/utilities/get-combined-array.ts
@@ -0,0 +1,31 @@
+import { Case } from '../constants/array-of-cases';
+import { Felony } from './array-of-felonies-01';
+
+type Payload = {
+  headings: Array<Case>;
+  items: Array<Felony>;
+};
+
+const getCombinedArrays = (payload: Payload) => {
+  const { headings, items } = payload;
+  const array = [];
+
+  for (let i = 0; i < headings.length; i++) {
+    // Create a heading element.
+    array.push({ ...headings[i], list: [], type: 'heading' });
+    const id = headings[i]['id'];
+
+    // Loop through the related list and create list items.
+    for (let j = 0; j < items.length; j++) {
+      const caseId = items[j]['caseId'];
+
+      if (caseId === id) {
+        array[i].list.push({ ...items[j], type: 'item' });
+      }
+    }
+  }
+
+  return array;
+};
+
+export default getCombinedArrays;
