0001c3dd24b85e3e84d26ac40b5d254624b71924
Rob Welan <rmw.its@gmail.com>
2023-09-13 13:06:11 +1000

    Update app felony charges

--- src/content-layout/side-drawer/link-items-apps.ts
+++ src/content-layout/side-drawer/link-items-apps.ts
@@ -1,9 +1,18 @@
 //  react-icons
 import { FaTasks } from '@react-icons/all-files/fa/FaTasks';
+import IconDonaldJTrump from '../../images/apps/felony-charges-of-donald-j-trump/noun-trump';
 //  constants
-import { PUBLIC_ROUTE_PAGE_APPS_TO_DO } from '../../security/constants/routes-public';
+import {
+  PUBLIC_ROUTE_APPS_FELONY_CHARGES_DJT,
+  PUBLIC_ROUTE_PAGE_APPS_TO_DO,
+} from '../../security/constants/routes-public';
 
 const linkItems = [
+  {
+    icon: IconDonaldJTrump,
+    name: 'Donald J Trump',
+    route: PUBLIC_ROUTE_APPS_FELONY_CHARGES_DJT,
+  },
   {
     icon: FaTasks,
     name: 'To Do',
