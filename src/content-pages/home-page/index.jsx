10577e75a32ddfcb0240a5b6f01e933d79428593
Rob Welan <rmw.its@gmail.com>
2023-11-19 13:37:45 +1000

    Update content home page
     - remove unecessary containers

--- src/content-pages/home-page/index.jsx
+++ src/content-pages/home-page/index.jsx
@@ -12,21 +12,15 @@ const ContentHomePage = (props) => {
   if (edges.length === 0) {
     return (
       <>
-        <Container maxW="6xl" sx={{ marginBottom: '2em' }}>
-          <Text>There are no posts. Please come back later.</Text>
-        </Container>
+        <Text>There are no posts. Please come back later.</Text>
       </>
     );
   }
 
   return (
     <>
-      <Container maxW="6xl" sx={{ marginBottom: '2em' }}>
-        <>
-          {searchResultsLength === 0 && <BlogPosts data={edges} />}
-          {searchResultsLength > 0 && <BlogPosts data={searchResults} />}
-        </>
-      </Container>
+      {searchResultsLength === 0 && <BlogPosts data={edges} />}
+      {searchResultsLength > 0 && <BlogPosts data={searchResults} />}
     </>
   );
 };
