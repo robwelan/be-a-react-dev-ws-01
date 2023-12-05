206ad6d3f24755535380588e2228f8d64a69879d
Rob Welan <rmw.its@gmail.com>
2023-12-05 21:51:11 +1000

    Add products
     - new magent

--- src/constants/storefront-zazzle/magnet-designs/index.js
+++ src/constants/storefront-zazzle/magnet-designs/index.js
@@ -1,6 +1,11 @@
+import magnetDishwasherItIsYourJob from './magnet-dishwasher-it-is-your-job';
 import magnetSittingCat from './magnet-sitting-long-hair-cat';
 import magnetSittingDog from './magnet-sitting-short-hair-dog';
 
-const products = [magnetSittingCat, magnetSittingDog];
+const products = [
+  magnetDishwasherItIsYourJob,
+  magnetSittingCat,
+  magnetSittingDog,
+];
 
 export default products;
---
+++ src/constants/storefront-zazzle/magnet-designs/magnet-dishwasher-it-is-your-job.js
@@ -0,0 +1,20 @@
+/*
+   <div style="text-align:center;line-height:150%"> <a href="https://www.zazzle.com.au/dishwasher_clean_it_is_your_job_magnet-256116176618996644" rel="nofollow" > <img src="https://rlv.zcache.com.au/dishwasher_clean_it_is_your_job_magnet-r6a554deaa810490e9e85fdcda2f271eb_ambom_8byvr_325.jpg?bg=0xffffff" alt="Dishwasher clean? It is Your Job! Magnet" style="border:0;" /> </a> <br /> <a href="https://www.zazzle.com.au/dishwasher_clean_it_is_your_job_magnet-256116176618996644" rel="nofollow" >Dishwasher clean? It is Your Job! Magnet</a> <br />by <a href="https://www.zazzle.com.au/store/beareactdev" rel="nofollow">beareactdev</a> </div>
+*/
+
+const label = `Dishwasher: It is your job!`;
+
+const magnet = {
+  asset: {
+    image: {
+      alt: label.toLowerCase(),
+      src: 'https://rlv.zcache.com.au/dishwasher_clean_it_is_your_job_magnet-r6a554deaa810490e9e85fdcda2f271eb_ambom_8byvr_325.jpg?bg=0xffffff',
+    },
+    name: label,
+    uri: 'https://www.zazzle.com.au/dishwasher_clean_it_is_your_job_magnet-256116176618996644',
+  },
+  name: label,
+  type: 'magnet',
+};
+
+export default magnet;
