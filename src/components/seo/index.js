52d25e3e7aad146f42aca44f7dd4ac2074ec3313
Rob Welan <robwelan@yahoo.com.au>
2024-04-18 11:40:42 +1000

    Update seo for Bing Clarity

--- src/components/seo/index.js
+++ src/components/seo/index.js
@@ -77,13 +77,13 @@ const Seo = (props) => {
       {/* canonical authority */}
       <link rel="canonical" href={uri}></link>
       {/* <!-- Clarity tracking code for https://beareact.dev/ --> */}
-      <script>
-      (function(c,l,a,r,i,t,y){
+      <script type="text/javascript">
+    (function(c,l,a,r,i,t,y){
         c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i+"?ref=bwt";
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
         y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-      })(window, document, "clarity", "script", "lyaid6vzxr");
-      </script>
+    })(window, document, "clarity", "script", "lyaid6vzxr");
+</script>
       {/* children */}
       {children}
     </>
