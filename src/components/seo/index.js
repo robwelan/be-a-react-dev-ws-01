c2f6b12735b6087b64bf3dd61dcb1534fb5758be
Rob Welan <robwelan@yahoo.com.au>
2024-04-18 12:41:32 +1000

    Remove clarity
     - code is shit

--- src/components/seo/index.js
+++ src/components/seo/index.js
@@ -76,14 +76,6 @@ const Seo = (props) => {
       )}
       {/* canonical authority */}
       <link rel="canonical" href={uri}></link>
-      {/* <!-- Clarity tracking code for https://beareact.dev/ --> */}
-      <script type="text/javascript">
-    (function(c,l,a,r,i,t,y){
-        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-    })(window, document, "clarity", "script", "lyaid6vzxr");
-</script>
       {/* children */}
       {children}
     </>
