59e616893acd9c69c10080205cacd0ac64938cc3
Rob Welan <robwelan@yahoo.com.au>
2024-04-29 09:46:08 +1000

    Update weather app
     - minor fixes

--- src/content-apps/weather/index.tsx
+++ src/content-apps/weather/index.tsx
@@ -1,5 +1,7 @@
 import React, { lazy, Suspense } from 'react';
+//  chakra-ui
 import { Text } from '@chakra-ui/react';
+//  react error boundary
 import { ErrorBoundary } from 'react-error-boundary';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 //  components
@@ -11,7 +13,7 @@ const queryClient = new QueryClient();
 
 const Content = () => {
   return (
-    <ErrorBoundary fallback={<Text>Something went wrong</Text>}>
+    <ErrorBoundary fallback={<Text>Something went wrong.</Text>}>
       <QueryClientProvider client={queryClient}>
         <Suspense fallback={<LoadingScreen />}>
           <GeoLocation />
--- src/content-apps/weather/view/weather-by-location/index.tsx
+++ src/content-apps/weather/view/weather-by-location/index.tsx
@@ -1,27 +1,6 @@
 import React from 'react';
-//  recoil
-// import { useRecoilValue } from 'recoil';
-//  state
-// import {
-//   weatherLocationForecast,
-//   weatherLocationLocale,
-// } from '../../state/atoms';
-//  local components
 import TopCard from './top-card';
 
-const WeatherByLocation = () => {
-  // const forecast = useRecoilValue(weatherLocationForecast);
-  // const locale = useRecoilValue(weatherLocationLocale);
-
-  // if (forecast.loading) {
-  //   return null;
-  // }
-
-  // if (locale.loading) {
-  //   return null;
-  // }
-
-  return <TopCard />;
-};
+const WeatherByLocation = () => <TopCard />;
 
 export default WeatherByLocation;
