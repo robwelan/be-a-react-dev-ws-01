b4cd0ba61b685e35460bfb683be88bb65838ddbb
Rob Welan <robwelan@yahoo.com.au>
2024-06-07 09:46:01 +1000

    Update emojis
     - create-items now uses fake-api

--- src/content-apps/emoji/view/emojis/infinite-scroll/utilities/create-items.ts
+++ src/content-apps/emoji/view/emojis/infinite-scroll/utilities/create-items.ts
@@ -1,24 +1,53 @@
-import { TypeEmoji } from '../../../../state/types';
+//  recoil
+import { useRecoilValue } from 'recoil';
+//  state
+import { emojiFakeAPI } from '../../../../state/atoms';
 
 type createItemsPayload = {
-  api: {
-    data: Array<TypeEmoji>;
-  };
   rows: {
     first: number;
     last: number;
   };
 };
 
-const createItems = (payload: createItemsPayload) => {
+const createItems = async (payload: createItemsPayload) => {
   const {
-    api: { data = [] },
     rows: { first = 0, last = 50 },
   } = payload;
+  const resultObject = {
+    error: false,
+    data: {
+      emojis: [],
+      error: {},
+      length: 0,
+    },
+  };
 
-  const items = data.slice(first, last);
+  try {
+    const api = useRecoilValue(emojiFakeAPI);
 
-  return items;
+    const items = api.emojis.slice(first, last);
+
+    return {
+      ...resultObject,
+      data: {
+        ...resultObject.data,
+        emojis: items,
+        length: items && items.length ? items.length : 0,
+      },
+    };
+  } catch (error) {
+    console.error(error);
+
+    return {
+      ...resultObject,
+      error: true,
+      data: {
+        ...resultObject.data,
+        error,
+      },
+    };
+  }
 };
 
 export default createItems;
