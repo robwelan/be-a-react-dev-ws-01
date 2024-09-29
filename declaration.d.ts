30064d126a3ed374be33d1abce654cd639f2ab58
Rob Welan <robwelan@yahoo.com.au>
2024-09-29 14:33:18 +1000

    Update declarations.d.ts file
     - add module css support

--- declaration.d.ts
+++ declaration.d.ts
@@ -1,4 +1,8 @@
-declare module "*.png";
-declare module "*.svg";
-declare module "*.jpeg";
-declare module "*.jpg";
+declare module '*.png';
+declare module '*.svg';
+declare module '*.jpeg';
+declare module '*.jpg';
+declare module '*.module.css' {
+  const classes: { [key: string]: string };
+  export default classes;
+}
