85630b348c2f26ab9852bc785d6ccfca3f342b5b
Rob Welan <robwelan@yahoo.com.au>
2024-09-29 03:29:36 +0200

    Refactor content-layout scripts
     - remove cross-origin issue
     - trying to change background color of iframe content is no longer possible...
     - also, better window object checking before code is executed

--- src/content-layout/scripts-independent.tsx
+++ src/content-layout/scripts-independent.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 //  hooks
 import useScript from '../hooks/use-script';
+//  utlities
+import getWindow from '../utilities/window/get-window';
 
-const IndependentScripts = () => {
+const UseScript = () => {
   useScript({
     async: true,
     src: 'https://storage.ko-fi.com/cdn/scripts/overlay-widget.js',
@@ -10,6 +12,24 @@ const IndependentScripts = () => {
   });
 
   return <></>;
+};
+
+const IndependentScripts = () => {
+  const [isWindow, setIsWindow] = useState(false);
+  const windowGlobal = getWindow();
+
+  //  isWindow effect
+  useEffect(() => {
+    if (windowGlobal) {
+      setIsWindow(true);
+    }
+
+    // return () => setIsWindow(false);
+  }, [windowGlobal]);
+
+  if (!isWindow) return null;
+
+  return <UseScript />;
 };
 
 export default IndependentScripts;
