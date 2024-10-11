e00174a3de3df86ea3371707f09ec659a3916393
robwelan <robwelan@yahoo.com.au>
2024-10-11 11:08:48 +1000

    Make Tutorials Links Consistent with Game Links

--- src/constants/page-links/tutorials/games/rock-paper-scissors.ts
+++ src/constants/page-links/tutorials/games/rock-paper-scissors.ts
@@ -1,12 +1,12 @@
 //  react icons
-import { RiGameLine } from 'react-icons/ri';
+import { FaRegHandScissors } from 'react-icons/fa';
 //  routes
 import { PUBLIC_ROUTE_PAGE_TUTORIALS_GAME_ROCK_PAPER_SCISSORS } from '../../../../security/constants/routes-public';
 
 const game = {
   description:
     'In this tutorial you will be building a working rock-paper-scissors game with React and TypeScript.',
-  icon: RiGameLine,
+  icon: FaRegHandScissors,
   name: 'Build a Rock Paper Scissors Game with React and TypeScript',
   route: PUBLIC_ROUTE_PAGE_TUTORIALS_GAME_ROCK_PAPER_SCISSORS,
 };
--- src/content-pages/tutorials/rock-paper-scissors-game-with-react/index.tsx
+++ src/content-pages/tutorials/rock-paper-scissors-game-with-react/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 //  chakra-ui
 import { Box, Center, Heading, List, Text } from '@chakra-ui/react';
 //  react icons
-import { RiGameLine } from 'react-icons/ri';
+import { FaRegHandScissors } from 'react-icons/fa';
 //  components
 import ListItemWithLink from '../../../components/list-item-with-link';
 import LinkToTutorials from '../components/link-to-tutorials';
@@ -23,77 +23,77 @@ const ContentTutorial = () => (
     <Box sx={{ height: '1em' }} />
     <List>
       <ListItemWithLink
-        icon={RiGameLine}
+        icon={FaRegHandScissors}
         label="Game State Rock Paper Scissors"
         route="/game-state-rock-paper-scissors/"
       />
       <ListItemWithLink
-        icon={RiGameLine}
+        icon={FaRegHandScissors}
         label="Game Entry Rock Paper Scissors"
         route="/game-entry-rock-paper-scissors/"
       />
       <ListItemWithLink
-        icon={RiGameLine}
+        icon={FaRegHandScissors}
         label="Game View - Game Heading Rock Paper Scissors"
         route="/game-view-game-heading-rock-paper-scissors/"
       />
       <ListItemWithLink
-        icon={RiGameLine}
+        icon={FaRegHandScissors}
         label="Game View - Game Wrapper Rock Paper Scissors"
         route="/game-view-game-wrapper-rock-paper-scissors/"
       />
       <ListItemWithLink
-        icon={RiGameLine}
+        icon={FaRegHandScissors}
         label="Game View - Game Play Heading Rock Paper Scissors"
         route="/game-view-play-heading-rock-paper-scissors/"
       />
       <ListItemWithLink
-        icon={RiGameLine}
+        icon={FaRegHandScissors}
         label="Game View - Game Players Rock Paper Scissors"
         route="/game-view-players-rock-paper-scissors/"
       />
       <ListItemWithLink
-        icon={RiGameLine}
+        icon={FaRegHandScissors}
         label="Game View - User Rock Paper Scissors"
         route="/game-view-user-rock-paper-scissors/"
       />
       <ListItemWithLink
-        icon={RiGameLine}
+        icon={FaRegHandScissors}
         label="Game View - Computer Rock Paper Scissors"
         route="/game-view-computer-rock-paper-scissors/"
       />
       <ListItemWithLink
-        icon={RiGameLine}
+        icon={FaRegHandScissors}
         label="Game View - Game Results Rock Paper Scissors"
         route="/game-view-results-rock-paper-scissors/"
       />
       <ListItemWithLink
-        icon={RiGameLine}
+        icon={FaRegHandScissors}
         label="Game View - Game Controls Resets Rock Paper Scissors"
         route="/game-view-controlsresets-rock-paper-scissors/"
       />
       <ListItemWithLink
-        icon={RiGameLine}
+        icon={FaRegHandScissors}
         label="Game Action - User Decision Rock Paper Scissors"
         route="/game-action-set-decision-user-rock-paper-scissors/"
       />
       <ListItemWithLink
-        icon={RiGameLine}
+        icon={FaRegHandScissors}
         label="Game Action - Computer Decision Rock Paper Scissors"
         route="/game-action-set-decision-computer-rock-paper-scissors/"
       />
       <ListItemWithLink
-        icon={RiGameLine}
+        icon={FaRegHandScissors}
         label="Game Action - Get Result Rock Paper Scissors"
         route="/game-action-get-result-rock-paper-scissors/"
       />
       <ListItemWithLink
-        icon={RiGameLine}
+        icon={FaRegHandScissors}
         label="Game Action - Set Clear Game Rock Paper Scissors"
         route="/game-action-set-clear-game-rock-paper-scissors/"
       />
       <ListItemWithLink
-        icon={RiGameLine}
+        icon={FaRegHandScissors}
         label="Game Action - Set Clear All Rock Paper Scissors"
         route="/game-action-set-clear-game-all-rock-paper-scissors/"
       />
