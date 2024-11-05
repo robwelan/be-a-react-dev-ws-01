7373ab9a42fc3b2c4a62ad9a3b8d6284423ba31f
robwelan <robwelan@yahoo.com.au>
2024-11-06 08:17:03 +1000

    Update home page
     - better app descriptions (hopefully)

--- src/constants/page-links/link-items-apps.ts
+++ src/constants/page-links/link-items-apps.ts
@@ -15,7 +15,7 @@ import {
 
 const linkItems = [
   {
-    description: 'A quick reference of standard emojis to search and copy.',
+    description: 'A quick reference of standard emojis to search and Copy to Clipboard feature to help you quickly use them.',
     icon: MdOutlineEmojiSymbols,
     name: 'Emojis',
     route: PUBLIC_ROUTE_PAGE_APPS_EMOJI_DICTIONARY,
@@ -35,7 +35,7 @@ const linkItems = [
   },
   {
     description:
-      'Handy set of useful HTML code snippets with a copy to clipboard feature for ease-of-use.',
+      'Handy set of useful HTML codes, symbols and entities with a Copy to Clipboard feature to help you quickly use them.',
     icon: MdOutlineEmojiSymbols,
     name: 'Useful HTML Entities',
     route: PUBLIC_ROUTE_PAGE_APPS_USEFUL_HTML_ENTITIES,
